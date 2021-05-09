f9007503c10a413c295885e21b9bfa8248d9f80c
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-05-09 14:41:51 -0400

    image transformations

---
+++ transformations007.py
@@ -0,0 +1,63 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('Photos/park.jpg')
+
+cv.imshow('Park', img)
+
+# Translation
+def translate(img, x, y):
+    transMat = np.float32([[1,0,x],[0,1,y]])
+    dimensions = (img.shape[1], img.shape[0])
+    return cv.warpAffine(img, transMat, dimensions)
+
+# -x ---> Left
+# -y ---> Up
+# x ---> Right
+# y ---> Down
+
+translated = translate(img, -100, 100)   # shift 100 left, 100 down
+# translated = translate(img, 100, 100)   # shift 100 right, 100 down
+cv.imshow('Translated', translated)
+
+
+# Rotation
+def rotate(img, angle, rotPoint=None):
+    (height, width) = img.shape[:2]
+
+    if rotPoint is None:
+        rotPoint = (width//2,height//2)
+    
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, scale=1.0)     # we are not messing with scale this time
+    dimensions = (width,height)
+
+    return cv.warpAffine(img, rotMat, dimensions)
+
+rotated = rotate(img, -45)
+# rotated = rotate(img, 45)
+cv.imshow('Rotated', rotated)
+
+rotated_rotated = rotate(rotated, -45)
+cv.imshow('Rotated Rotated', rotated_rotated)   # notice the back triangeles from previous rotated
+
+
+# Resizing
+resized = cv.resize(img, (500,500), interpolation=cv.INTER_CUBIC)
+cv.imshow('Resized', resized)
+
+
+# Flipping
+flip = cv.flip(img, 0)
+cv.imshow('Flip', flip)
+# 0 - flip vertically over the x axis
+# 1 - flip horizontally over the y axis
+#-1 - flip on both x and y
+
+
+# Cropping
+cropped = img[200:400, 300:400]
+cv.imshow('Cropped', cropped)
+
+
+
+cv.waitKey(0)
