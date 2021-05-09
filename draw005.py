6dd5cf0ab505cd8f12a67eed3baa260b32186ad0
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-05-08 22:57:59 -0400

    Drawing shapes and Putting Text

---
+++ draw005.py
@@ -0,0 +1,35 @@
+import cv2 as cv
+import numpy as np
+
+blank = np.zeros((500,500,3), dtype='uint8')  # uint8 is a data type of an img
+# height, width, # of color channels(RGB)
+cv.imshow('Blank', blank)
+
+
+# # 1. Paint the image a certain color
+# blank[200:300, 300:400] = 0,255,0      # Green
+# # blank[:] = 0,255,0      # Green
+# # blank[:] = 0,0,255      # Red
+# cv.imshow('Green', blank)
+
+# 2. Draw a Rectangle
+# cv.rectangle(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (0,255,0), thickness=cv.FILLED)   # thickness=-1 is same result
+# # cv.rectangle(blank, (0,0), (250,250), (0,0,255), thickness=2)
+# cv.imshow('Rectangle', blank)
+
+# # 3. Draw a Circle
+# cv.circle(blank, (blank.shape[1]//2, blank.shape[0]//2), 40, (0,0,255), thickness=-1)
+# # cv.circle(blank, (blank.shape[1]//2, blank.shape[0]//2), 40, (0,0,255), thickness=3)
+# cv.imshow('Circle', blank)
+
+# # 4. Draw a Line
+# cv.line(blank, (100,250), (300,400), (255,255,255), thickness=3)
+# # cv.line(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (255,255,255), thickness=3)
+# cv.imshow('Line', blank)
+
+
+# 5. Write text
+cv.putText(blank, 'Hello, my name\'s Jeff...', (0,225), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0,255,0), thickness=2)
+cv.imshow('Text', blank)
+
+cv.waitKey(0)
